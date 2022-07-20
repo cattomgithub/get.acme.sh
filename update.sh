10c776f39c7c2bbcf07a331d6592d87d43b60086
cattomgithub <linweijie2015@gmail.com>
2022-07-20 18:14:45 +0800

    Update from source

--- update.sh
+++ update.sh
@@ -2,10 +2,27 @@
 
 # For local PC! #
 
+#记录重试次数
+Retry_Count=0
+
 cd public || exit
+
+proxychains wget https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
 
 proxychains wget https://github.com/acmesh-official/acme.sh/archive/master.tar.gz
 
-git add . && git commit -a -m "Update master.tar.gz"
+git add . && git commit -a -m "Update from source"
 
-proxychains git push
+while ((Retry_Count <= 5)); do
+    if ! proxychains git push; then
+        echo "Pull Succeed!"
+        break
+    else
+        echo "Pull Failed!Retry it!"
+        ((Retry_Count++)) || true
+        if ((Retry_Count == 5)); then
+            echo "We have retried five times!It's time to give up."
+            exit 1
+        fi
+    fi
+done
